67ef0ede412a68ef00ef28407c1b18563b90105b
= <=>
2025-06-17 11:53:02 +0530

    updated Script Code

--- P-1/script.js
+++ P-1/script.js
@@ -1,13 +1,62 @@
 
+let users = [];
+let user = {};
+
+const validateUser = () => {
+    let email = document.querySelector("#loginEmail").value;
+    let pass = document.querySelector("#loginPassword").value;
+    const found = users.find(value => value.email === email && value.pass === pass);
+    if(found){
+        showHome();
+    }
+    else{
+        document.getElementById("errTxt").innerHTML = "Access Denied";
+    }
+}
+
+const displayUser = () => {
+    if(users.length === 0){
+        return `<p>No user Found</p>`;
+    }
+    let html = "";
+    users.forEach(user => {
+        html += `<p>User Name: ${user.name} <br> User Email: ${user.email}`;
+    });
+    return html;
+}
+
+
 const loginForm = () => {
-    const str = ` <div>
+    const str = ` <div display = 'flex'>
         <h3> Login Form </h3>
+        <p id = "errTxt"></p>
         <p>Email: <input type="email" id="loginEmail" /></p>
         <p>Password: <input type="password" id="loginPassword" /></p>
-        <p> <button onclick = 'showHome()'> Submit </button> </p>
+        <p> <button onclick = 'validateUser()'> Submit </button> </p>
         <p> <button onclick = 'registerForm()'> Create Account </button> </p>
+
+        <div id = "display">
+            <h4> List of users: </h4>
+            ${displayUser()}
+         </div>
     `;
     root.innerHTML = str + "</div>";
+}
+
+const saveUser = () => {
+    let name = document.querySelector("#registerName").value;
+    let email = document.querySelector("#registerEmail").value;
+    let pass = document.querySelector("#registerPassword").value;
+    // user.name = name;
+    // user.email = email;
+    // user.pass = pass;
+    users.push({
+        name,
+        email,
+        pass
+    });
+
+    loginForm();
 }
 
 const registerForm = () => {
@@ -16,7 +65,7 @@ const registerForm = () => {
         <p>Name: <input type="text" id="registerName"/></p>
         <p>Email: <input type="email" id="registerEmail" /></p>
         <p>Password: <input type="password" id="registerPassword" /></p>
-        <p> <button onclick = "loginForm()"> Submit </button> </p>
+        <p> <button onclick = "saveUser()"> Submit </button> </p>
         <p> <button onclick = "loginForm()"> Already a member? Click here to login... </button> </p>
     `;
     root.innerHTML = str + "</div>";
