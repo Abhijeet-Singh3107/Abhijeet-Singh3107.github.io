19ac67c6c0d13db63c4b256479017b31282bd24c
= <=>
2025-06-20 10:01:29 +0530

    Starting WIth JavaScript

---
+++ JS/file1.js
@@ -0,0 +1,27 @@
+console.log("Starting with JavaScript...");
+
+let studentName = "Abx";
+let age = 21;
+let promoted = true;
+let score;
+let city = null;
+
+// console.log(studentName);
+// console.log(age);
+// console.log(promoted);
+// console.log(typeof score);      // undefined
+// console.log(typeof city);       // --> typeof null is object
+
+// console.log(typeof age);
+// let strAge = String(age);
+// console.log(typeof strAge);
+
+// let rating = "5";
+// console.log(typeof rating);
+// let numRating = Number(rating);
+// console.log(typeof numRating);
+
+let flag = "true";
+let boolFlag = Boolean(flag);
+console.log(boolFlag);
+console.log(typeof boolFlag);
